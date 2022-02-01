1560de2e371f074c50974590c1a6f10a4bb440b5
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2022-02-01 08:17:18 +0700

    Adding read and update category

--- app/category/controller.js
+++ app/category/controller.js
@@ -3,7 +3,11 @@ const Category = require("./model");
 module.exports = {
     index: async (req, res) => {
         try {
-            res.render("admin/categories/view_category");
+            const categories = await Category.find();
+
+            res.render("admin/categories/view_category", {
+                categories,
+            });
         } catch (err) {
             console.log(err);
         }
@@ -23,6 +27,32 @@ module.exports = {
 
             let category = await Category({ name });
             await category.save();
+
+            res.redirect("/category");
+        } catch (err) {
+            console.log(err);
+        }
+    },
+
+    viewEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const category = await Category.findOne({ _id: id });
+
+            res.render("admin/categories/edit", {
+                category,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+
+    actionEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { name } = req.body;
+
+            const category = await Category.findOneAndUpdate({ _id: id }, { name });
 
             res.redirect("/category");
         } catch (err) {
