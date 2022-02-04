0bc00541bbfe5f239759bd5d88aa487462403caf
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2022-02-04 18:07:34 +0700

    update views

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,7 +1,23 @@
+const Transaction = require("../transaction/model");
+const Voucher = require("../voucher/model");
+const Player = require("../player/model");
+const Category = require("../category/model");
+
 module.exports = {
     index: async (req, res) => {
         try {
-            res.render("index", {
+            const transactions = await Transaction.count();
+            const vouchers = await Voucher.count();
+            const players = await Player.count();
+            const categories = await Category.count();
+
+            res.render("admin/dashboard/view_dashboard", {
+                count: {
+                    transactions,
+                    vouchers,
+                    players,
+                    categories,
+                },
                 name: req.session.user.name,
                 active: "dashboard",
             });
